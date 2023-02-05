ae1d5cf28d13c5e489df584ace92c6d97bd246dd
sgamba2 <114009519+sgamba2@users.noreply.github.com>
2023-02-05 11:31:13 +0100

    Delete graphical__utilities_8h.js

--- graphical__utilities_8h.js
+++
@@ -1,8 +0,0 @@
-var graphical__utilities_8h =
-[
-    [ "afbhist", "graphical__utilities_8h.html#a402ad752672030c62dd862061c67a8a0", null ],
-    [ "coshisto", "graphical__utilities_8h.html#a15195e6daf9addf3ac419878684ba047", null ],
-    [ "dmmasshisto", "graphical__utilities_8h.html#a07716b57818dbe5ff9b5bae853be6963", null ],
-    [ "operationhist", "graphical__utilities_8h.html#a7681d891e25dbae1760d1f14e531aee9", null ],
-    [ "save_histogram", "graphical__utilities_8h.html#af49ad1827056366916483538e9583c48", null ]
-];
